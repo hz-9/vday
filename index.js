3b5b48c436c03d2a3db3821b5213c29ea0227446
HeinHtetZaw <hzawnineseven@gmail.com>
2024-02-14 07:15:07 +0630

    [projec]

---
+++ index.js
@@ -0,0 +1,18 @@
+new TypeIt("#multipleStrings", {
+  strings: [
+    "Smile, a simple curve in your face",
+    "That sets everything into place",
+    "It's a gesture in your lips",
+    "That makes me forget how",
+    "to sleep...",
+    " ",
+    "Smiling will cost you nothing",
+    "But for me it means everything",
+    "It always happen in just a flash",
+    "Yet the memory stored in me",
+    "will last...",
+    "-Euphrosyne",
+  ],
+  speed: 70,
+  waitUntilVisible: true,
+}).go();
